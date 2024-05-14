ec67644fe71866f20bb7e299de157923bc2aec36
TUTULROY <tutulroyjoy@gmail.com>
2024-05-15 04:36:03 +0600

    added toggling

--- src/Queries/Queries.jsx
+++ src/Queries/Queries.jsx
@@ -17,12 +17,26 @@ const Queries = () => {
         } )
     },[])
 //    console.log(queries);
+const [gridLayout, setGridLayout] = useState(3); 
+
+const toggleGridLayout = (columns) => {
+  setGridLayout(columns);
+};
     
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div>
+            <div role="tablist" className="tabs tabs-boxed">
+            <div role="tablist" className="tabs tabs-boxed">
+                <button onClick={() => toggleGridLayout(2)} className={`tab ${gridLayout === 2 ? 'tab-active' : ''}`}>2 Columns</button>
+                <button onClick={() => toggleGridLayout(3)} className={`tab ${gridLayout === 3 ? 'tab-active' : ''}`}>3 Columns</button>
+               
+            </div>
+        </div>
+        <div className={`grid grid-cols-${gridLayout} sm:grid-cols-2 md:grid-cols-3 gap-4`}>
            {
             queries.map(query => <AllQuery key={query._id} query={query}></AllQuery>)
            }
+        </div>
         </div>
     );
 };
